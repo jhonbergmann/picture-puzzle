bbd0bfd367ad228a368799a309fb78f03814f09c
Jhonatan Bergmann <54135650+jhonbergmann@users.noreply.github.com>
2025-02-19 17:46:05 -0300

    app

---
+++ app.tsx
@@ -0,0 +1,12 @@
+import {StatusBar} from 'expo-status-bar'
+
+import Dashboard from './src/screens/dashboard'
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" hidden />
+      <Dashboard />
+    </>
+  )
+}
